73e5dbcaee9c81da861b3460a629c980a13ff3b1
Amir Ayed <146146233+amirayed7@users.noreply.github.com>
2024-06-09 22:26:34 -0400

    Strings, and Algorithms

---
+++ 6kyu/Stop_gninnipS_My_sdroW!.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples:
+
+// "Hey fellow warriors"  --> "Hey wollef sroirraw" 
+// "This is a test        --> "This is a test" 
+// "This is another test" --> "This is rehtona test"
+
+//My answer
+const spinWords = str => (result => (str.split(' ').forEach(x => result.push(x.length < 5 ? x : x.split('').reverse().join(''))), result.join(' ')))([]);
